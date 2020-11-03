0b3ddca6d5140e01b56444f05fdcdd1e4d114125
TIKJA <najaroen_tik@live.com>
2020-11-03 11:35:09 +0700

    init-file

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express()
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(3001, ()=> { console.log('running')})
